e5457fc65c92f8b3bc7ffbe2870a03dc248bc3b1
King Dav <mickyyoung660@gmail.com>
2026-09-06 00:25:20 +0100

    Guard MONA font set to avoid CI missing Inversionz.otf error

--- main.py
+++ main.py
@@ -176,7 +176,11 @@ def main():
     t.gen_text("\x1b[92mfetch.sh\x1b[0m", 1, contin=True)
     t.gen_typing_text(" -u callmidavid", 1, contin=True)
 
-    t.set_font(FONT_FILE_MONA, 16, 0)
+    try:
+        if os.path.isfile(FONT_FILE_MONA) and os.path.getsize(FONT_FILE_MONA) >= 1024:
+            t.set_font(FONT_FILE_MONA, 16, 0)
+    except Exception:
+        pass
     t.toggle_show_cursor(False)
     monaLines = r"""
     \x1b[49m     \x1b[90;100m}}\x1b[49m     \x1b[90;100m}}\x1b[0m
